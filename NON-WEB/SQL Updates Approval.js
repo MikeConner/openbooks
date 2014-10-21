23f0ff5c1cce08b2841ef4ab78186c894c0c229c
Arash <arash.danaie@gmail.com>
2014-10-21 10:05:45 -0400

    sql updates

--- NON-WEB/SQL Updates Approval.js
+++ NON-WEB/SQL Updates Approval.js
@@ -1,1 +1,31 @@
-﻿
+﻿USE [CityController]
+GO
+
+/****** Object:  StoredProcedure [dbo].[SetUserRoles]    Script Date: 10/17/2014 7:47:18 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+-- =============================================
+-- Author:		Zeo
+-- Create date: 10/17/14
+-- Description:	Set Permission Roles for User
+-- =============================================
+CREATE PROCEDURE [dbo].[SetUserRoles]
+	@UserName NVARCHAR(50),
+	@PermissionGroup NVARCHAR(50)
+AS 
+BEGIN
+    UPDATE users
+	SET PermissionGroup = @PermissionGroup
+	WHERE UserName = @UserName
+
+RETURN 
+
+
+END
+
+GO
