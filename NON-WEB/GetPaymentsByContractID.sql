e54cd341aae38e0d345a67d16ea638f4d7cebca6
arash <arash@arashdanaie.com>
2017-01-07 22:39:57 -0500

    db sql file fixes as deployed

--- NON-WEB/GetPaymentsByContractID.sql
+++ NON-WEB/GetPaymentsByContractID.sql
@@ -29,3 +29,60 @@ END
 GO
 
 
+
+
+
+
+-- =============================================
+-- Author:		Zeo
+-- Create date: 07/16/09
+-- Description:	Get a Contract
+-- =============================================
+ALTER PROCEDURE [dbo].[GetContractByContractID] 
+	@ContractID NVARCHAR(50),
+	@SupplementalNo INT
+AS
+BEGIN
+--select * from contracts where vendorno is null
+--select * from vendors where vendorno = '0000254011'
+--exec GetContractByContractID 48451, 0
+	SET NOCOUNT ON;
+
+	SELECT c.ContractID,
+		c.VendorNo,
+		vendors.VendorName,
+	    sv.VendorNo AS SecondVendorNo,
+		sv.VendorName AS SecondVendorName,
+		c.DepartmentID,
+		c.IndexCode,
+		c.SupplementalNo,
+		c.ResolutionNo,
+		c.[Service],
+		c.Amount,
+		c.OriginalAmount,
+		c.[Description],
+		c.Notes,
+		c.Comments,
+		c.DateSolicitor,
+		c.DateDuration,
+		c.DateCountersigned,
+		c.DateEntered,
+		c.AmountReceived,
+		tlk_service.ServiceName, 
+		tlk_department.DeptName,
+		CASE
+			WHEN oc.ContractID IS NULL THEN 'False'
+			ELSE 'True'
+		END AS 'HasPdf'
+	FROM contracts c  
+	JOIN tlk_service ON c.Service = tlk_service.ID 
+	INNER JOIN vendors ON c.VendorNo = vendors.VendorNo 
+    LEFT OUTER JOIN vendors sv ON c.SecondVendorNo = sv.VendorNo 
+	LEFT OUTER JOIN tlk_department ON c.DepartmentID = tlk_department.DeptCode 
+	LEFT OUTER JOIN tblOnbaseContracts oc ON c.ContractID = oc.ContractID 
+	WHERE c.ContractID= @ContractID AND c.SupplementalNo = @SupplementalNo
+
+END
+
+
+
